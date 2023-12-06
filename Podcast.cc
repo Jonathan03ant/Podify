a276a39e2c85b967ffb3cf2de8bc2c07fec90e45
student <jonathan03ant@gmail.com>
2023-12-06 12:21:34 -0500

    Podcast class update

---
+++ Podcast.cc
@@ -0,0 +1,32 @@
+#include "Podcast.h"
+
+Podcast::Podcast(const string& title, const string& host)
+    :title(title), host(host) {}
+
+bool Podcast::equals(const string& title) const{
+    return this->title == title;
+}
+
+Episode* Podcast::get(int index) {
+    return episodes[index];
+}
+
+void Podcast::print(ostream& os) const{
+    os << "Title: "<< title << ", Host: " << host;
+}
+
+void Podcast::printWithEpisode(ostream& os) const{
+    print(os);
+    for (int i = 0; i <episodes.getSize(); i++){
+        os << *episodes[i];
+    }
+}
+
+void Podcast::add(Episode* episode){
+    episodes += episode;
+}
+
+ostream& operator<<(ostream& os, const Podcast& podcast){
+    podcast.print(os);
+    return os;
+}
---
+++ Podcast.h
@@ -0,0 +1,32 @@
+#ifndef PODCAST_H
+#define PODCAST_H
+
+#include "Episode.h"
+#include "Array.h"
+#include <string>
+#include <stdio.h>
+#include <ostream>
+
+using namespace std;
+
+class Podcast {
+    public:
+        Podcast(const string& title, const string& host);
+
+
+        bool equals(const string& title) const;
+        Episode* get(int index);
+        int getSide() const;
+        void print(ostream& os) const;
+        void printWithEpisode(ostream& os) const;
+        void add(Episode* episode);
+
+        friend ostream& operator<<(ostream& os, const Podcast& podcast);
+
+    private:
+        string title;
+        string host;
+        Array<Episode*> episodes;
+}
+
+#endif
